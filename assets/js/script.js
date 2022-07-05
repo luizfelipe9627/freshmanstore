7ae333c5d54daa9d27520a345a8d78907b051814
Luiz Felipe <felipesilva9627@hotmail.com>
2022-07-05 01:45:03 -0300

    Link das imagens do menu burguer arrumadas.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 document.querySelector(".menu-mobile").addEventListener("click", function () {
   if (document.querySelector(".menu").style.opacity == "100") {
     document.querySelector(".menu").style.opacity = "0";
-    document.querySelector(".menu-mobile").src = "https://freshmanstore.github.io/freshmanstore/assets/image/menu-mobile.svg";
+    document.querySelector(".menu-mobile").src = "https://freshmanstore.github.io/assets/image/menu-mobile.svg";
   } else {
     document.querySelector(".menu").style.opacity = "100";
-    document.querySelector(".menu-mobile").src = "https://freshmanstore.github.io/freshmanstore/assets/image/fechar-menu.svg";
+    document.querySelector(".menu-mobile").src = "https://freshmanstore.github.io/assets/image/fechar-menu.svg";
   }
 });
 
